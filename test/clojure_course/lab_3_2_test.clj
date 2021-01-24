013e27210668eeb1fe16509572f1b30c701b6a33
Anastasiya Shadrina <a.shadrina5@mail.ru>
2021-01-24 14:42:48 +0700

    Add time test

--- test/clojure_course/lab_3_2_test.clj
+++ test/clojure_course/lab_3_2_test.clj
@@ -15,4 +15,41 @@
       (is (= (filter even? empty-l) (pfilter-lazy even? empty-l)))
       (is (= (filter long-running-pred l1) (pfilter-lazy long-running-pred l1)))
       (is (= (filter long-running-pred l2) (pfilter-lazy long-running-pred l2)))
-      (is (= (filter long-running-pred empty-l) (pfilter-lazy long-running-pred empty-l))))))
+      (is (= (filter long-running-pred empty-l) (pfilter-lazy long-running-pred empty-l)))
+      (is (= (take 10 (filter even? naturals)) (take 10 (pfilter-lazy even? naturals)))))))
+
+(deftest time-test-with-regular-pred-lazy
+  (testing "testing filter vs pfilter-lazy with regular predicate"
+    (let [l1 (list 1 2 3 4 5 6 7 8)
+          l2 (list 3 7 1 5 8 4 2 6)]
+      (println "load classes:")
+      (time (println (pfilter-lazy even? (range 0 100))))
+      (println)
+      (println "with filter:")
+      (time (println (filter even? l1)))
+      (time (println (filter even? l2)))
+      (time (println (take 10 (filter even? naturals))))
+      (println)
+      (println "with pfilter:")
+      (time (println (pfilter-lazy even? l1)))
+      (time (println (pfilter-lazy even? l2)))
+      (time (println (take 10 (pfilter-lazy even? naturals)))))))
+
+(deftest time-test-with-long-running-pred-lazy
+  (testing "testing filter vs pfilter-lazy with long running predicate"
+    (let [l1 (list 1 2 3 4 5 6 7 8)
+          l2 (list 3 7 1 5 8 4 2 6)]
+      (println "load classes:")
+      (time (println (pfilter-lazy long-running-pred (range 0 10))))
+      (println)
+      (println "with filter:")
+      (time (println (filter long-running-pred l1)))
+      (time (println (filter long-running-pred l2)))
+      (time (println (filter long-running-pred (range 100))))
+      (time (println (take 10 (filter long-running-pred naturals))))
+      (println)
+      (println "with pfilter:")
+      (time (println (pfilter-lazy long-running-pred l1)))
+      (time (println (pfilter-lazy long-running-pred l2)))
+      (time (println (pfilter-lazy long-running-pred (range 100))))
+      (time (println (take 10 (pfilter-lazy long-running-pred naturals)))))))
